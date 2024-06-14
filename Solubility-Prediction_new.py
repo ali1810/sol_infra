60d5439d39f103b9435d2abdc36d48453799f0ff
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-06-14 13:27:57 +0200

    Update Solubility-Prediction_new.py

--- Solubility-Prediction_new.py
+++ Solubility-Prediction_new.py
@@ -2,6 +2,13 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 import home,project,contact
+import sys
+
+st.title("Check Python Version")
+
+python_version = sys.version
+st.write(f"Python version: {python_version}")
+
 
 
 
