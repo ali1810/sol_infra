83008608a838cb5ed42f7e56f354f1fc2ddb3f5b
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-06-15 08:09:12 +0200

    Update Solubility-Prediction.py

--- Solubility-Prediction.py
+++ Solubility-Prediction.py
@@ -4,5 +4,11 @@ from streamlit_option_menu import option_menu
 import home,project,contact
 
 import streamlit as st
+import sys
+
+st.title("Check Python Version....Samjhe ")
+
+python_version = sys.version
+st.write(f"Python version: {python_version}")
 
 #st.header("🧪🧪🧪Aquous Solubility Prediction📏📏📏 ")
